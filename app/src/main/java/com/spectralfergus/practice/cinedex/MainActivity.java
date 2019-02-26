d9ef91e62052290147a4c7a29471004213eeae07
SpectralFergus <SpectralFergus@gmail.com>
2019-02-25 21:50:23 -0800

    Added Android Studio Project

---
+++ app/src/main/java/com/spectralfergus/practice/cinedex/MainActivity.java
@@ -0,0 +1,14 @@
+package com.spectralfergus.practice.cinedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
