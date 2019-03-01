10c71dc05c118c05be2db3237373db02a388ce0e
SpectralFergus <SpectralFergus@gmail.com>
2019-03-01 13:59:43 -0800

    Added packages, organized by feature

---
+++ app/src/main/java/com/spectralfergus/cinedex/movies/MoviesActivity.java
@@ -0,0 +1,16 @@
+package com.spectralfergus.cinedex.movies;
+
+import android.os.Bundle;
+
+import com.spectralfergus.cinedex.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
