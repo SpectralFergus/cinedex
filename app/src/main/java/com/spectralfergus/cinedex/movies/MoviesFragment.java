d330798e572de8a773d7e2bc1481aa70ce9e77fb
SpectralFergus <SpectralFergus@gmail.com>
2019-03-02 16:27:03 -0800

    Added basic impl & tool previews for Movies Activity/Fragment

--- app/src/main/java/com/spectralfergus/cinedex/movies/MoviesFragment.java
+++ app/src/main/java/com/spectralfergus/cinedex/movies/MoviesFragment.java
@@ -11,6 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.spectralfergus.cinedex.R;
+import com.spectralfergus.cinedex.data.TMDBMovie;
+
+import java.util.List;
 
 
 /**
@@ -21,7 +24,7 @@ import com.spectralfergus.cinedex.R;
  * Use the {@link MoviesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MoviesFragment extends Fragment {
+public class MoviesFragment extends Fragment implements MoviesContract.View {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -41,18 +44,10 @@ public class MoviesFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MoviesFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static MoviesFragment newInstance(String param1, String param2) {
-        MoviesFragment fragment = new MoviesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static MoviesFragment newInstance() {
+        return new MoviesFragment();
     }
 
     @Override
@@ -79,21 +74,41 @@ public class MoviesFragment extends Fragment {
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void setProgressIndicator(boolean active) {
+
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void showNotes(List<TMDBMovie> notes) {
+
     }
+
+    @Override
+    public void showAddNote() {
+
+    }
+
+    @Override
+    public void showNoteDetailUi(String noteId) {
+
+    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
 
     /**
      * This interface must be implemented by activities that contain this
